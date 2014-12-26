6a48bed72a6b3b8b96c4b55cf118cc47015d518c
Christopher Bartling <chris.bartling@gmail.com>
2014-12-26 00:34:44 -0600

    Now pulling Gravatar avatar image.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,15 @@
+require 'digest/md5'
+
 class WelcomeController < ApplicationController
 
   before_action :authenticate_user!
 
   def index
-    render view: 'index', layout: 'authenticated-application'
+    hash = Digest::MD5.hexdigest(current_user.email.downcase)
+    profile_image_src = "http://www.gravatar.com/avatar/#{hash}?size=60"
+    render view: 'index',
+           layout: 'authenticated-application',
+           locals: { profile_image_src: profile_image_src }
   end
 
 end
