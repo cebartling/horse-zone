88a0da2d9f24b3b337fd17bcf8e948d521a399f8
Christopher Bartling <chris.bartling@gmail.com>
2015-02-22 12:34:47 -0600

    Adding shell script for version controlling the CodeShip setup.

---
+++ bin/codeship-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export RUBY_VERSION="ruby-2.2.0"
+
+rvm install $RUBY_VERSION
+rvm use "$RUBY_VERSION@horse-zone" --create
+bundle install
+bundle exec rake db:create
+bundle exec rake db:schema:load
+
+
