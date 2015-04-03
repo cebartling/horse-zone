a27d394c228d367c732be97fb6563a3a5f40f227
Christopher Bartling <chris.bartling@gmail.com>
2015-04-03 07:27:14 -0500

    Adding specs around use cases before using them.

--- spec/use_cases/user_management/create_new_user_spec.rb
+++ spec/use_cases/user_management/create_new_user_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe UserManagement::CreateNewUser do
-
   describe '#execute' do
-    let(:use_case) {UserManagement::CreateNewUser.new}
+    let(:use_case) { UserManagement::CreateNewUser.new }
 
     before :each do
       @outcome = use_case.execute(params)
@@ -17,6 +16,18 @@ RSpec.describe UserManagement::CreateNewUser do
       it 'should execute successfully' do
         expect(@outcome.success?).to be_truthy
       end
+
+      it 'result should be new user' do
+        expect(@outcome.result).to be_instance_of User
+      end
+
+      it 'new user should have email address set' do
+        expect(@outcome.result.email_address).to eq params[:email_address]
+      end
+
+      it 'new user should have digested password set' do
+        expect(@outcome.result.password_digest).not_to be_nil
+      end
     end
 
     context 'invalid parameters' do
@@ -24,9 +35,16 @@ RSpec.describe UserManagement::CreateNewUser do
         let(:params) {
           {:password => 'MyP4$$w0rD'}
         }
+        let(:expected_messages) {
+          {email_address: ["can't be blank"]}
+        }
 
         it 'should fail execution' do
           expect(@outcome.success?).to be_falsey
+        end
+
+        it 'contains message about missing email address' do
+          expect(@outcome.failure.errors.messages).to eq expected_messages
         end
       end
 
@@ -34,9 +52,16 @@ RSpec.describe UserManagement::CreateNewUser do
         let(:params) {
           {:email_address => 'joe.smith@mycompany.com'}
         }
+        let(:expected_messages) {
+          {password: ["can't be blank"]}
+        }
 
         it 'should fail execution' do
           expect(@outcome.success?).to be_falsey
+        end
+
+        it 'contains message about missing password' do
+          expect(@outcome.failure.errors.messages).to eq expected_messages
         end
       end
     end
