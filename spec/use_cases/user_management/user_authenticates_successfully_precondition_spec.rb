511ad40ea82b75b7953da6651fe0b3bd008dd889
Christopher Bartling <chris.bartling@gmail.com>
2015-04-06 12:43:27 -0500

    Refactoring preconditions and getting specs around these newly refactored pre-conditions.

---
+++ spec/use_cases/user_management/user_authenticates_successfully_precondition_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe UserManagement::UserAuthenticatesSuccessfullyPrecondition do
+
+  describe '#satisfied?' do
+    let(:pre_condition) { UserManagement::UserAuthenticatesSuccessfullyPrecondition.new }
+    let(:existing_user) { FactoryGirl.create(:user) }
+    let(:outcome) {pre_condition.satisfied?(input)}
+
+    context 'pre-existing user email address parameter' do
+      let(:input) do
+        UserManagement::SignInUserInput.new({email_address: existing_user.email_address,
+                                             password: 'Pa$$w0rd'})
+      end
+
+      it 'causes pre-condition to pass' do
+        expect(outcome).to be_truthy
+      end
+    end
+
+    context 'non-existent user email address parameter' do
+      let(:input) do
+        UserManagement::SignInUserInput.new({email_address: existing_user.email_address,
+                                             password: 'Pa$$w0rdxdfjsldh'})
+      end
+
+      it 'causes pre-condition fails' do
+        expect(outcome).to be_falsey
+      end
+    end
+  end
+
+  describe 'self.symbol' do
+    it "should be equal to :user_authenticates_successfully" do
+      expect(UserManagement::UserAuthenticatesSuccessfullyPrecondition.symbol).to eq :user_authenticates_successfully
+    end
+  end
+end
