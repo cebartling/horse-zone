d80ab84b6e05cd8dc7816200e4fe372f63138bef
Christopher Bartling <chris.bartling@gmail.com>
2015-04-24 07:26:06 -0500

    Fixing bundler issue on CodeShip.

--- bin/codeship-setup.sh
+++ bin/codeship-setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash --login
 
-export RUBY_VERSION="ruby-2.2.1"
+export RUBY_VERSION="ruby-2.2.2"
 
 #rvm install $RUBY_VERSION
 rvm use "$RUBY_VERSION@horse-zone" --create
+gem update --system
+gem install bundler
 bundle install
 bundle exec rake db:create
 bundle exec rake db:schema:load
