fb839b4ab886949108165b87dc63b83480a50068
Christopher Bartling <chris.bartling@gmail.com>
2015-04-24 07:44:02 -0500

    Fixing bundler issue on CodeShip.

--- bin/codeship-setup.sh
+++ bin/codeship-setup.sh
@@ -6,6 +6,7 @@ export RUBY_VERSION="ruby-2.2.2"
 rvm use "$RUBY_VERSION@horse-zone" --create
 gem update --system
 gem install bundler
+gem install nokogiri -v '1.6.6.2'
 bundle install
 bundle exec rake db:create
 bundle exec rake db:schema:load
