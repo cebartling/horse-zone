d8087fe103f3c18b820fa2858c034a7ffe8c0b5e
Christopher Bartling <chris.bartling@gmail.com>
2015-09-10 00:43:26 -0500

    Update to Ruby 2.2.3 and Rails 4.2.4.

--- docker/postgresql/install-port-mappings.sh
+++ docker/postgresql/install-port-mappings.sh
@@ -4,18 +4,20 @@ COLOR_LIGHT_GREEN='\033[1;32m'
 COLOR_OFF='\033[0m'
 MAPPED_PORT=49255
 
-printf "\n${COLOR_LIGHT_GREEN}=====> Shutting down boot2docker ...${COLOR_OFF}\n"
-boot2docker stop
-printf "\n${COLOR_LIGHT_GREEN}=====> The boot2docker process has been shut down.${COLOR_OFF}\n"
+printf "\n${COLOR_LIGHT_GREEN}=====> Shutting down docker-machine ...${COLOR_OFF}\n"
+docker-machine stop default
+printf "\n${COLOR_LIGHT_GREEN}=====> The docker-machine process has been shut down.${COLOR_OFF}\n"
 
-printf "\n${COLOR_LIGHT_GREEN}=====> Configuring boot2docker-vm port forwarding for port $MAPPED_PORT ...${COLOR_OFF}\n"
-VBoxManage modifyvm "boot2docker-vm" --natpf1 "tcp-port$MAPPED_PORT,tcp,,$MAPPED_PORT,,$MAPPED_PORT";
-VBoxManage modifyvm "boot2docker-vm" --natpf1 "udp-port$MAPPED_PORT,udp,,$MAPPED_PORT,,$MAPPED_PORT";
-printf "\n${COLOR_LIGHT_GREEN}=====> The boot2docker-vm port forwarding for port $MAPPED_PORT has been configured!${COLOR_OFF}\n"
+printf "\n${COLOR_LIGHT_GREEN}=====> Configuring 'default' VM port forwarding for port $MAPPED_PORT ...${COLOR_OFF}\n"
+VBoxManage modifyvm "default" --natpf1 "tcp-port$MAPPED_PORT,tcp,,$MAPPED_PORT,,$MAPPED_PORT";
+VBoxManage modifyvm "default" --natpf1 "udp-port$MAPPED_PORT,udp,,$MAPPED_PORT,,$MAPPED_PORT";
+printf "\n${COLOR_LIGHT_GREEN}=====> The 'default' VM port forwarding for port $MAPPED_PORT has been configured!${COLOR_OFF}\n"
 
-printf "\n${COLOR_LIGHT_GREEN}=====> Starting up boot2docker ...${COLOR_OFF}\n"
-boot2docker start
-printf "\n${COLOR_LIGHT_GREEN}=====> The boot2docker process has been started!${COLOR_OFF}\n"
+printf "\n${COLOR_LIGHT_GREEN}=====> Starting up docker-machine ...${COLOR_OFF}\n"
+docker-machine start default
+eval "$(docker-machine env default)"
+printf "\n${COLOR_LIGHT_GREEN}=====> The docker-machine process has been started!${COLOR_OFF}\n"
+
 
 
 
--- docker/postgresql/uninstall-port-mappings.sh
+++ docker/postgresql/uninstall-port-mappings.sh
@@ -4,12 +4,12 @@ COLOR_LIGHT_GREEN='\033[1;32m'
 COLOR_OFF='\033[0m'
 MAPPED_PORT=49255
 
-printf "\n${COLOR_LIGHT_GREEN}=====> Shutting down boot2docker ...${COLOR_OFF}\n"
-boot2docker stop
-printf "\n${COLOR_LIGHT_GREEN}=====> The boot2docker process has been shut down.${COLOR_OFF}\n"
+printf "\n${COLOR_LIGHT_GREEN}=====> Shutting down docker-machine ...${COLOR_OFF}\n"
+docker-machine stop default
+printf "\n${COLOR_LIGHT_GREEN}=====> The docker-machine process has been shut down.${COLOR_OFF}\n"
 
-printf "\n${COLOR_LIGHT_GREEN}=====> Removing boot2docker-vm port forwarding for port $MAPPED_PORT ...${COLOR_OFF}\n"
-VBoxManage modifyvm "boot2docker-vm" --natpf1 delete "tcp-port$MAPPED_PORT";
-VBoxManage modifyvm "boot2docker-vm" --natpf1 delete "udp-port$MAPPED_PORT";
-printf "\n${COLOR_LIGHT_GREEN}=====> The boot2docker-vm port forwarding for port $MAPPED_PORT has been removed!${COLOR_OFF}\n"
+printf "\n${COLOR_LIGHT_GREEN}=====> Removing 'default' VM port forwarding for port $MAPPED_PORT ...${COLOR_OFF}\n"
+VBoxManage modifyvm "default" --natpf1 delete "tcp-port$MAPPED_PORT";
+VBoxManage modifyvm "default" --natpf1 delete "udp-port$MAPPED_PORT";
+printf "\n${COLOR_LIGHT_GREEN}=====> The 'default' VM port forwarding for port $MAPPED_PORT has been removed!${COLOR_OFF}\n"
 
