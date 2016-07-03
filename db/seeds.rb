8c75d85cf3d0bd99ee4e1f345605d5fbe47b1a22
Christopher Bartling <chris.bartling@gmail.com>
2016-07-03 12:05:37 -0500

    Adding seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,14 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 if Role.where(lookup_code: 'OWNER').count == 0
-  Role.create(lookup_code: 'OWNER', )
+  Role.create(lookup_code: 'OWNER', name: 'owner')
+end
+
+if Role.where(lookup_code: 'INSTRUCTOR').count == 0
+  Role.create(lookup_code: 'INSTRUCTOR', name: 'instructor')
+end
+
+if Role.where(lookup_code: 'PARTICIPANT').count == 0
+  Role.create(lookup_code: 'PARTICIPANT', name: 'participant')
 end
