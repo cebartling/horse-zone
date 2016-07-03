46616d9935897212337de111c97d4da6a64d3800
Christopher Bartling <chris.bartling@gmail.com>
2016-07-03 02:54:40 -0500

    Tweaking the API routing.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+# *id*::            <tt>integer, not null, primary key</tt>
+# *first_name*::    <tt>string(30), not null</tt>
+# *last_name*::     <tt>string(40), not null</tt>
+# *email_address*:: <tt>string(200), not null</tt>
+# *created_at*::    <tt>datetime, not null</tt>
+# *updated_at*::    <tt>datetime, not null</tt>
+#--
+# == Schema Information End
+#++
+
 class User < ApplicationRecord
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope '/api/v1' do
+    resources :users
+  end
 end
+
+# == Route Map
+#
+# Prefix Verb   URI Pattern                 Controller#Action
+#  users GET    /api/v1/users(.:format)     users#index
+#        POST   /api/v1/users(.:format)     users#create
+#   user GET    /api/v1/users/:id(.:format) users#show
+#        PATCH  /api/v1/users/:id(.:format) users#update
+#        PUT    /api/v1/users/:id(.:format) users#update
+#        DELETE /api/v1/users/:id(.:format) users#destroy
+#
